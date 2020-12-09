1625e474a0b25fc42dd68762ed43dba28779bd44
Samyukta Nair <samycode00@gmail.com>
2020-12-09 21:55:58 +0530

    all

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Register from "./Registration/Registration";
+import Login from "./Registration/Login";
+import Feed from "./Feed";
+import { Route } from "react-router-dom";
+import Attendance from "./Attendance/Attendance";
+import UploadImage from "./Attendance/UploadImage";
+import GetInfo from "./Attendance/GetInfo";
+import AddStudent from "./Attendance/AddStudent";
+import Report from "./Attendance/Report";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Route exact path="/" component={Feed} /> */}
+
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/attendance" component={Attendance} />
+        <Route exact path="/image" component={UploadImage} />
+        <Route exact path="/getinfo" component={GetInfo} />
+        <Route exact path="/add" component={AddStudent} />
+        <Route exact path="/report" component={Report} />
+      </div>
+    );
+  }
+}
+
+export default App;
